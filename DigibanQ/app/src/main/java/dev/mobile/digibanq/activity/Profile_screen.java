a2a728d7b55f5badf35cf357b7b9e7f5b964a115
Benjamin Nkansah <kwanka55@gmail.com>
2016-12-21 02:03:42 +0000

    update

---
+++ DigibanQ/app/src/main/java/dev/mobile/digibanq/activity/Profile_screen.java
@@ -0,0 +1,115 @@
+package dev.mobile.digibanq.activity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.mobile.digibanq.adapter.SimpleRecyclerAdapter;
+import dev.mobile.digibanq.model.VersionModel;
+import dev.mobile.digibanq.utils.Utils;
+import info.androidhive.digibanq.R;
+
+/**
+ * Created by banktech on 12/5/2016.
+ */
+
+public class Profile_screen extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbar;
+    RecyclerView recyclerView;
+    int mutedColor = R.attr.colorPrimary;
+    SimpleRecyclerAdapter simpleRecyclerAdapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.profile_screen);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.anim_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("");
+
+        ImageView header = (ImageView) findViewById(R.id.header);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.header);
+
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @SuppressWarnings("ResourceType")
+            @Override
+            public void onGenerated(Palette palette) {
+
+                mutedColor = palette.getMutedColor(R.color.primary_500);
+                collapsingToolbar.setContentScrimColor(mutedColor);
+                collapsingToolbar.setStatusBarScrimColor(R.color.black_trans80);
+            }
+        });
+
+        recyclerView = (RecyclerView) findViewById(R.id.scrollableview);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        List<String> listData = new ArrayList<String>();
+        int ct = 0;
+        for (int i = 0; i < VersionModel.data.length * 2; i++) {
+            listData.add(VersionModel.data[ct]);
+            ct++;
+            if (ct == VersionModel.data.length) {
+                ct = 0;
+            }
+        }
+
+        if (simpleRecyclerAdapter == null) {
+            simpleRecyclerAdapter = new SimpleRecyclerAdapter(listData);
+            recyclerView.setAdapter(simpleRecyclerAdapter);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_pager, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
